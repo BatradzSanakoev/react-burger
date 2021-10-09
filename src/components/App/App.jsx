4a5aeabe00f67c3e56e9fd09cf9ce711c6399d91
Bato <sanakoev.batraz@yandex.ru>
2021-10-09 16:28:52 +0300

    feat: разработать хэдер

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppHeader from '../AppHeader/AppHeader';
+
+const App = () => {
+    return (
+        <>
+            <AppHeader />
+        </>
+    )
+};
+
+export default App;
+
